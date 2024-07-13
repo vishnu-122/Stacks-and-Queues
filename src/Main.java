5efcf8a796f5725a7efbec394c7e1fb07961ed25
vishnu-122 <146123549+vishnu-122@users.noreply.github.com>
2024-07-13 19:06:04 -0400

    added RPN calculator

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -11,5 +13,31 @@ public class Main {
         System.out.println("Top element: "+ newStack.peek());
         System.out.println("popped Element: " + newStack.pop());
         System.out.println("Stack after pop: " + newStack);
+
+        //creating RPN calaculator which works on stack as well
+        Stack<Double> stack = new Stack<>();
+        try {
+            Scanner sin = new Scanner(System.in);
+            System.out.print("Enter Expression: ");
+            while (sin.hasNextLine()) {
+                String expression = sin.nextLine();
+                for (String token : expression.split(" ")) {
+                    System.out.print(token + "\t\t");
+                    switch (token) {
+                        case "+":
+                            System.out.println("Adding top two elements\t\t");
+                            stack.push(stack.pop() + stack.pop());
+                            break;
+                        default:
+                            System.out.println("Pushing " + token);
+                            stack.push(Double.parseDouble(token));
+                    }
+                    System.out.println(stack); // Uses toString to see Stack
+                }
+                System.out.print("Enter Expression: ");
+            }
+        } catch (Exception err) {
+            System.out.println(err.getMessage());
+        }
     }
 }
